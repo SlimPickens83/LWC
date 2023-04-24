474b0cb56b26c6182b970ea756c0f93e35cc6c3a
Mike Radcliffe <mikerad83@gmail.com>
2023-04-24 12:51:55 -0400

    Edit button now updates database

--- JS/todo-App/public/browser.js
+++ JS/todo-App/public/browser.js
@@ -1,7 +1,7 @@
 document.addEventListener("click", function(e) {
     if (e.target.classList.contains("edit-me")) {
         let userInput = prompt("Enter updated text:")
-        axios.post("/update-item", {text: userInput}).then(function() {
+        axios.post("/update-item", {text: userInput, id: e.target.getAttribute("data-id")}).then(function() {
             // do something later
         }).catch(function() {
             console.log("Please try again later.")
