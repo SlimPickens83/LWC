4506cc14fbc43ffd6bbcfcc6bdfc11913624a429
SlimPickens83 <mikerad83@gmail.com>
2023-04-25 16:13:36 -0400

    Added email validation

--- JS/complex-app/models/User.js
+++ JS/complex-app/models/User.js
@@ -1,9 +1,22 @@
 let User = function(data) {
     this.data = data
+    this.errors = []
+}
+
+User.prototype.validate = function() {
+    if (this.data.username == "") {this.errors.push("You must provide a username.")}
+    if (this.data.email == "") {this.errors.push("You must provide a valid email address.")}
+    if (this.data.password == "") {this.errors.push("You must provide a password.")}
+    if (this.data.password.length > 0 && this.data.password.length < 12) {this.errors.push("Password must be at least 12 characters.")}
+    if (this.data.password.length > 100) {this.errors.push("Password cannot exceed 100 characters.")}
+    if (this.data.username.length > 0 && this.data.username.length < 3) {this.errors.push("Username must be at least 3 characters.")}
+    if (this.data.username.length > 30) {this.errors.push("Username cannot exceed 30 characters.")}
 }
 
 User.prototype.register = function() {
-    
+    // Step 1: Validate User Data
+    this.validate()
+    // Step 2: Only if no validation errors, then save user data to database.
 }
 
 module.exports = User
