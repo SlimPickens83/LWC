e7b35af4bd2897308965edc8f0aac9b2183c3eec
SlimPickens83 <mikerad83@gmail.com>
2023-04-25 16:18:41 -0400

    Updates to email validation

--- JS/complex-app/models/User.js
+++ JS/complex-app/models/User.js
@@ -1,3 +1,5 @@
+const validator = require("validator")
+
 let User = function(data) {
     this.data = data
     this.errors = []
@@ -5,7 +7,8 @@ let User = function(data) {
 
 User.prototype.validate = function() {
     if (this.data.username == "") {this.errors.push("You must provide a username.")}
-    if (this.data.email == "") {this.errors.push("You must provide a valid email address.")}
+    if (this.data.username != "" && !validator.isAlphanumeric(this.data.username)) {this.errors.push("Username can only contain letters and numbers.")}
+    if (!validator.isEmail(this.data.email)) {this.errors.push("You must provide a valid email address.")}
     if (this.data.password == "") {this.errors.push("You must provide a password.")}
     if (this.data.password.length > 0 && this.data.password.length < 12) {this.errors.push("Password must be at least 12 characters.")}
     if (this.data.password.length > 100) {this.errors.push("Password cannot exceed 100 characters.")}
