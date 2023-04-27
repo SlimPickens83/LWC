8ebdb9a1c17a0f100e923064ec0b7945fa48a8a1
SlimPickens83 <mikerad83@gmail.com>
2023-04-27 15:31:13 -0400

    Added hashing for user passwords

--- JS/complex-app/models/User.js
+++ JS/complex-app/models/User.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcryptjs")
 const usersCollection = require("../db").collection("user-list")
 const validator = require("validator")
 
@@ -18,6 +19,7 @@ User.prototype.cleanUp = function() {
         email: this.data.email.trim().toLowerCase(),
         password: this.data.password
     }
+
 }
 
 User.prototype.validate = function() {
@@ -26,7 +28,7 @@ User.prototype.validate = function() {
     if (!validator.isEmail(this.data.email)) {this.errors.push("You must provide a valid email address.")}
     if (this.data.password == "") {this.errors.push("You must provide a password.")}
     if (this.data.password.length > 0 && this.data.password.length < 12) {this.errors.push("Password must be at least 12 characters.")}
-    if (this.data.password.length > 100) {this.errors.push("Password cannot exceed 100 characters.")}
+    if (this.data.password.length > 50) {this.errors.push("Password cannot exceed 50 characters.")}
     if (this.data.username.length > 0 && this.data.username.length < 3) {this.errors.push("Username must be at least 3 characters.")}
     if (this.data.username.length > 30) {this.errors.push("Username cannot exceed 30 characters.")}
 }
@@ -36,7 +38,7 @@ User.prototype.login = function() {
         this.cleanUp()
     const attemptedUser = await usersCollection.findOne({username: this.data.username})
 
-    if (attemptedUser && attemptedUser.password == this.data.password) {
+    if (attemptedUser && bcrypt.compareSync(this.data.password, attemptedUser.password)) {
         resolve("Congrats!")
     } else {
         reject("Invalid username / password.")
@@ -51,6 +53,9 @@ User.prototype.register = function() {
 
     // Step 2: Only if no validation errors, then save user data to database.
     if (!this.errors.length) {
+        // Hash user password
+        let salt = bcrypt.genSaltSync(10)
+        this.data.password = bcrypt.hashSync(this.data.password, salt)
         usersCollection.insertOne(this.data)
     }
     
